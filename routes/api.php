2d6b5ca10b70b613651d71866b0bb75a206ef1f5
EDMAR <edmarcrescencio856@gmail.com>
2025-10-24 14:19:54 +0800

    Remove property detail retrieval endpoint from PropertyApiController and corresponding route in API routes

--- routes/api.php
+++ routes/api.php
@@ -86,7 +86,6 @@ Route::prefix('properties')->middleware(['api', 'throttle:60,1'])->group(functio
     Route::get('/featured-properties', [PropertyApiController::class, 'getFeaturedProperties']);
     Route::get('/stats-properties', [PropertyApiController::class, 'getPropertyStats']);
     Route::get('/statuses-properties', [PropertyApiController::class, 'getAvailableStatuses']);
-    Route::get('/{id}', [PropertyApiController::class, 'show']);
 });
 
 // Site Settings API Routes - Public read access with rate limiting, protected write access
